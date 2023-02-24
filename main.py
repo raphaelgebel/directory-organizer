ee58fa8b9c40d76b96abce549b99b8ea512d5876
Raphael Gebel <raphael.gebe.@gmx.de>
2023-02-24 08:20:31 +0100

    made small adjustments to the code

--- main.py
+++ main.py
@@ -116,9 +116,9 @@ def decide_between_criteria():
     button_confirm_criteria.grid_forget()
 
     # Distinguishing between the possible options and execution of the respective function
-    if options.get() == "file type":
+    if options.get() == 'file type':
         organize_directory_by_file_type(directory_to_organize)
-    elif options.get() == "date":
+    elif options.get() == 'date':
         organize_directory_by_date(directory_to_organize)
     else:
         get_criteria_for_organisation()
@@ -137,7 +137,7 @@ def get_criteria_for_organisation():
     # if the button 'button_confirm_criteria' is pressed, then the function 'decide_between_criteria' is executed
 
 
-def start_the_program():
+def main():
     # Adding the widgets for entering the directory to the GUI root
     label_directory.grid(row=0, column=0, pady=10)
     entry_directory.grid(row=0, column=1, pady=10)
@@ -150,33 +150,33 @@ def start_the_program():
 
 # GUI root configuration
 root = tk.Tk()
-root.title("DIRECTORY ORGANIZER")
-root.geometry("510x50")
+root.title('DIRECTORY ORGANIZER')
+root.geometry('510x50')
 root.resizable(width=False, height=False)
 
 
 # Creating the different widgets that will be used in the program
 
 # Creating the widgets for entering the directory
-label_directory = tk.Label(root, text="directory: ")
+label_directory = tk.Label(root, text='directory: ')
 entry_directory = tk.Entry(root, width=50)
-button_confirm_directory = tk.Button(root, text="CONFIRM", command=get_criteria_for_organisation)
+button_confirm_directory = tk.Button(root, text='CONFIRM', command=get_criteria_for_organisation)
 
 # Creating the widgets for selecting the criteria by which the folder should be organized
-label_criteria = tk.Label(root, text="Select a criterion by which the directory should be organized: ")
-button_confirm_criteria = tk.Button(root, text="CONFIRM", command=decide_between_criteria)
+label_criteria = tk.Label(root, text='Select a criterion by which the directory should be organized: ')
+button_confirm_criteria = tk.Button(root, text='CONFIRM', command=decide_between_criteria)
 
 # Creating a dropdown menu for selecting the different criteria
 options = tk.StringVar()
-options.set("SELECT")
-dropdown_menu_criteria = tk.OptionMenu(root, options, "file type", "date")
+options.set('SELECT')
+dropdown_menu_criteria = tk.OptionMenu(root, options, 'file type', 'date')
 
 # Creating a label for confirming that the directory was organized
-label_confirm_organization = tk.Label(root, text="Your directory has successfully been organized.")
+label_confirm_organization = tk.Label(root, text='Your directory has successfully been organized.')
 
 # Creating a label for informing the user that an error has occurred
-label_show_error = tk.Label(root, text='Error: The system cannot find the specified directory!', fg="red")
+label_show_error = tk.Label(root, text='Error: The system cannot find the specified directory!', fg='red')
 
 
-# Running the function that starts the program
-start_the_program()
+if __name__ == "__main__":
+    main()
